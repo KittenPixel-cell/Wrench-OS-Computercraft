2c67e3b7fcb9ff927d2f9b8f47cde5373d89a86c
KittenPixel <79544605+KittenPixel-cell@users.noreply.github.com>
2023-01-01 14:27:12 -0600

    Update BSOD.lua

--- Wrench/API/BSOD.lua
+++ Wrench/API/BSOD.lua
@@ -17,10 +17,13 @@ local logo = {
 functionInit(msg)
     term.clear()
     term.setCursorPos(1,1)
+    term.setBackgroundColor(colors.blue
+    term.setTextColor(colors.white)
     for i = 1,12 do
         print(logo[i])
     end
     print(" ")
     term.setCursorPos(1,15)
+    term.setTextColor(colors.red)
     print("BSOD: Critical Error detected: "..msg)
 end

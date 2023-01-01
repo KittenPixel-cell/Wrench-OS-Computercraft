380b8c74a116d40e95d61b96515c3987e19d2f4d
KittenPixel <79544605+KittenPixel-cell@users.noreply.github.com>
2022-12-31 18:52:35 -0600

    Update Boot.lua

--- Boot.lua
+++ Boot.lua
@@ -3,62 +3,49 @@ if not term.isColor() then
 end
 
 local API = {
-    [1] = {"SHA256", "Wrench/API/sha256.lua"},
+    [1] = {"SHA256", "sha256"},
     [2] = {"GuiScreens", "Wrench/API/GuiScreens.lua"}
 }
 
-local total
+local total = 0
 local updateBar = function()
-    local step = #API / 37
-    total = total +step
+    local step = 37 / #API
+    total = total + step
     local round = math.floor(total)
-    if round >= 3 and round <= 37 then
-        paintutils.drawLine(3, 17, round, 17, colors.cyan)
-    end
-    term.setBackgroundColor(colors.white)
+    paintutils.drawLine(8, 17, 6+ round, 17, colors.cyan)
+    term.setBackgroundColor(colors.black)
 end
-
---local cPrint = functionlocal function centerPrint(text, ny)
-   -- if type(text) == "table" then
- --   for _, e in pairs(text) do centerPrint(e) 
-   -- end
---else
-  --  local x, y = term.getCursorPos()
-    --local w,h = term.getSize()
-    --term.setCursorPos(w/2 - text:len()/2, ny or y)
-    --print(text)
---end*/
 
 local logo = "Wrench OS"                                            
 
-term.setBackgroundColor(colors.white)
 term.setTextColor(colors.orange)
 
 term.clear()
 term.setCursorPos(1,1)
 for i = 1,1 do 
-    write("Wrench OS")
+    write("                    Wrench OS")
 end
 
-paintutils.drawLine(2,16,50,16,colors.blue)
-paintutils.drawLine(2,16,50,18,colors.blue)
-paintutils.drawLine(2,16,50,18,colors.blue)
-paintutils.drawLine(50,16,50,18,colors.blue)
+paintutils.drawLine(7,16,44,16,colors.blue)
+paintutils.drawLine(7,18,44,18,colors.blue)
+paintutils.drawLine(7,16,7,18,colors.blue)
+paintutils.drawLine(44,16,44,18,colors.blue)
 
-term.setBackgroundColor(colors.white)
 term.setTextColor(colors.lime)
 for i = 1, #API do
-    os.unloadAPI("Wrench/API/sha256.lua")
-    os.loadAPI("Wrench/API/sha256.lua")
+    os.unloadAPI(API[2][2])
+    os.loadAPI(API[2][2])
     term.setCursorPos(1,15)
     term.clearLine()
-    print("Loading API:"..API[1])
+    print("                    Loading OS")
+    sleep(2)
+    print("           Loading API: GuiScreens")
     --cPrint("Loading API:"..API[1])
     updateBar()
-
 end
 
-sleep(0)
+sleep(4)
 term.clear()
+term.setCursorPos(1,1)
 print("Boot screen loaded.")
   
